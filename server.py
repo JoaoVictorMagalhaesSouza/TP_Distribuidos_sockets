1814b22301a57261901f3a8fdb7512f318319e27
JoaoVictorMagalhaesSouza <joao.souza5@ufv.br>
2021-09-06 15:02:12 -0300

    servidor de testes

--- server.py
+++ server.py
@@ -24,4 +24,36 @@ class Server():
     def _service(self,con,cliente):
         #Método dos serviços do servidor (banco de dados)
         print(f"Atendendo o cliente {cliente}")
+        operadores = ["+","-","*","/"]
+        while True:
+            try:
+                mensagem = con.recv(104) #Recebendo a mensagem do cliente, dados brutos, fluxo de bytes
+                mensagem_decodificada = str(mensagem.decode("ascii")) #Bytes representam caracteres
+                #É importante criar uma formatação da mensagem enviada pelo cliente
+                #Exemplo: 15+5 onde 15 é o operando1, "+" operador e 5 o operando2
+                #Logo, a mensagem é da forma: operando1operadoroperando2
+                for x in operadores:
+                    if mensagem_decodificada.find(x) > 0:
+                        op = x
+                        mensagem_decodificada.split(op)
+                        break
+                if op=="+":
+                    resposta = float(mensagem_decodificada[0]) + float(mensagem_decodificada[1])
+                elif op=="-":
+                    resposta = float(mensagem_decodificada[0]) - float(mensagem_decodificada[1])
+                elif op=="*":
+                    resposta = float(mensagem_decodificada[0]) * float(mensagem_decodificada[1])
+                elif op=="/":
+                    resposta = float(mensagem_decodificada[0]) / float(mensagem_decodificada[1])
+                else:
+                    resposta = "Operação inválida"
 
+                con.send(bytes(str(resposta),"ascii")) #Converter a resposta para bytes também.
+                print(f"{cliente} -> requisição atendida !")
+
+            except OSError as os: #Erros de conexão (envio ou recebimento dos dados, divisão por 0, algum caractere invalido)
+                print(f"Erro na conexão {cliente}:{os.args}")
+                return 
+            except Exception as e:
+                print(f"Erro nos dados recebidos do cliente {cliente}:{e.args}")
+                con.send(bytes("Erro","ascii"))
